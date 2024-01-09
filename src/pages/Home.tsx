35e63ac4f34eb46b101d96b214b2ce826e40455b
TechClouds207 <139922172+TechClouds207@users.noreply.github.com>
2024-01-10 00:14:11 +0530

    fixing homepage error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import Button from "../components/ui-elements/Button";
-
-const Home = () => {
-  const [count, setCount] = useState(0);
-
-  // const minusHandler = () => { count === 0 ? setCount(0) : setCount(count - 1) }
-
-  const plusHandler = () => { 
-   // setCount(count + 1) 
-   alert('Button Clicked')
-  }
-  
+const Home = () => {  
   return (
     <>
-      <Button title="Add to Cart" event={plusHandler} />
+      Home of something very good!
     </>
   )
 }

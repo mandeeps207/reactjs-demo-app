a973f45b2c609265ce1133aa6dbe395f9a017a71
TechClouds207 <139922172+TechClouds207@users.noreply.github.com>
2024-01-12 01:05:19 +0530

    curved navbar code added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,6 @@ interface Nav {
   name: string
 }
 
-// const boxShadow: any = ({
-//   boxShadow: "-3px -3px 3px rgba(0, 0, 0, 0.25), 3px 3px 3px rgba(255, 255, 255, 1)"
-// })
-
 const Navbar = () => {
   const navs: Nav[] = [
     {id: 1, name: "about"},
@@ -16,11 +12,11 @@ const Navbar = () => {
     {id: 3, name: "blog"}
   ];
   return (
-    <nav className="flex flex-wrap items-end justify-between mx-auto shadow-sm bg-gradient-to-l from-gray-600 from-10% via-gray-500 via-30% to-gray-700 to-90%">
+    <nav className="flex flex-wrap items-end justify-between mx-auto shadow-sm bg-gray-600">
       <NavLink className="p-4 text-3xl text-white" to="/">LOGO</NavLink>
       <ul className="font-medium flex flex-row uppercase">
-        <li><NavLink to="/" className="block px-4 py-2 text-white rounded-t-lg">home</NavLink></li>
-        {navs.map(nav => <li key={nav.id}><NavLink className="block px-4 py-2 text-white rounded-t-lg" to={nav.name}>{nav.name}</NavLink></li>)}
+        <li><NavLink to="/" className="block px-10 py-1 text-white">home</NavLink></li>
+        {navs.map(nav => <li key={nav.id}><NavLink className="block px-10 py-1 text-white" to={nav.name}>{nav.name}</NavLink></li>)}
       </ul>
     </nav>
   )
